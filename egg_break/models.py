965b6425190480073ae5d5a88283281630fb6cce
Alexander <alexslabkin@gmail.com>
2020-03-12 23:53:39 +0300

    Styled button, half of login system, reqs

---
+++ egg_break/models.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+
+from egg_break import login, db
+
+
+class User(UserMixin, db.Model):
+    username = db.Column(db.String(16), primary_key=True)
+
+    def __str__(self):
+        return f'User: {username}'
+
+    def get_id(self):
+        return self.username
+
+
+@login.user_loader
+def load_user(username):
+    print(f'USER is {User.query.get(str(username))}')
+    return User.query.get(str(username))
