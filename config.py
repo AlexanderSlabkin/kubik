124a323b6d2f71886ee3a0b1279c3bc1bd93e310
Alexander <alexslabkin@gmail.com>
2020-04-01 22:46:50 +0300

    update reqs

--- config.py
+++ config.py
@@ -11,14 +11,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
      SECRET_KEY = os.getenv('SECRET_KEY') or 'very_hard_key'
      DEBUG = True
-     print(f'From dotenv: {os.getenv("DATABASE_URL")}')
+#     print(f'From dotenv: {os.getenv("DATABASE_URL")}')
 #    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
 #     'sqlite:///' + os.path.join(basedir, 'prog.db')
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
-     POSTGRES_USER = os.getenv('POSTGRES_USER')
-     POSTGRES_PW = os.getenv('POSTGRES_PW')
-     POSTGRES_URL = os.getenv('POSTGRES_URL')
-     POSTGRES_DB = os.getenv('POSTGRES_DB')
+#     POSTGRES_USER = os.getenv('POSTGRES_USER')
+#     POSTGRES_PW = os.getenv('POSTGRES_PW')
+#     POSTGRES_URL = os.getenv('POSTGRES_URL')
+#     POSTGRES_DB = os.getenv('POSTGRES_DB')
+     POSTGRES_DB = os.getenv('DATABASE_URL')
      if not (POSTGRES_DB and POSTGRES_PW and POSTGRES_URL and POSTGRES_USER):
          print('ENV ERROR')
      SQLALCHEMY_DATABASE_URI = \
